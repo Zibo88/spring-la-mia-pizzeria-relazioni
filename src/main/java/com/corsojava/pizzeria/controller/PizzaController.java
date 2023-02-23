2bb971d666cec4656d6a1f0df9ace10d41d59312
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-02-23 16:45:40 +0100

    relazione many to many

--- src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
+++ src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.corsojava.pizzeria.model.Ingredient;
 import com.corsojava.pizzeria.model.Pizza;
+import com.corsojava.pizzeria.repository.IngredientRepository;
 import com.corsojava.pizzeria.repository.PizzaRepository;
 
 import jakarta.validation.Valid;
@@ -25,6 +27,9 @@ public class PizzaController {
 	//repository
 	@Autowired
 	private  PizzaRepository pizzaRepository;
+	
+	@Autowired
+	private IngredientRepository ingredientRepository;
 	
 	//index
 	@GetMapping
@@ -44,7 +49,9 @@ public class PizzaController {
 	@GetMapping("/{id}")
 	public String show(@PathVariable("id") Integer id, Model model) {
 		Pizza  singolaPizza = pizzaRepository.getReferenceById(id);
+	
 		model.addAttribute("pizza",singolaPizza);
+	
 		return "pizza/pizzaDetail";
 	}
 	
@@ -52,6 +59,8 @@ public class PizzaController {
 	@GetMapping("/create")
 	public String create(Model model) {
 		Pizza newPizza = new Pizza();
+		List<Ingredient> allIngredient = ingredientRepository.findAll();
+		model.addAttribute("allIngredients", allIngredient);
 		model.addAttribute("pizza" ,newPizza);
 		return "pizza/create";
 	}
@@ -72,6 +81,8 @@ public class PizzaController {
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable("id") Integer id, Model model) {
 		Pizza pizzaToUpdate = pizzaRepository.getReferenceById(id);
+		List<Ingredient> allIngredient = ingredientRepository.findAll();
+		model.addAttribute("allIngredients", allIngredient);
 		model.addAttribute("pizzaToUpdate",pizzaToUpdate);
 		return "pizza/edit";
 	}
